a66ce843e8c1b1bfd7e7df4bbe35f3ef28b64718
SrAtom210 <mratom210@gmail.com>
2025-09-18 10:54:39 -0600

    I1: Se realizaron los primeros cambios a la UI

--- app/src/main/java/com/example/magfind/MainActivity.kt
+++ app/src/main/java/com/example/magfind/MainActivity.kt
@@ -4,13 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.magfind.ui.theme.MagFindTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +37,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MagFindTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background){
+                    Principal()
                 }
             }
         }
@@ -31,17 +48,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Principal(){
+    Column(modifier = Modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        Row (modifier = Modifier,
+            verticalAlignment = Alignment.CenterVertically){
+            var texto1 by remember { mutableStateOf(value = "") }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MagFindTheme {
-        Greeting("Android")
+            OutlinedTextField(
+                value = texto1,
+                onValueChange = { nuevoTexto -> texto1 = nuevoTexto },
+                label = { Text(text = "Categoria") },
+                placeholder = { Text(text = "Escriba la categoria") }
+            )
+        }
+
+        Row (modifier = Modifier.padding(20.dp)){
+            Button(onClick = {
+            },
+                colors = ButtonDefaults.buttonColors(Color.DarkGray))
+            {
+                Text(text = "Crear Categoria", color = Color.White)
+            }
+        }
+
     }
 }
